6756b5b6a858701535820efba7d6a2091d566ae6
Andrei Avram <avramandrei@ymail.com>
2019-02-03 22:20:00 +0200

    Private members.

--- lib/apixu/client.rb
+++ lib/apixu/client.rb
@@ -4,41 +4,17 @@ require 'json'
 
 module Apixu
   class Client
-    attr_reader :key
-
     API_URL = "https://api.apixu.com"
     API_VERSION = "v1"
     FORMAT = "json"
     HTTP_TIMEOUT = 20
     DOC_WEATHER_CONDITIONS_URL = "https://www.apixu.com/doc/Apixu_weather_conditions"
 
-    def initialize key=nil
-      @key = key || ENV["APIXU_KEY"]
+    def initialize key
+      @key = key
 
-      unless @key
+      unless ! @key.to_s.strip.empty?
         raise Errors::InvalidKey
-      end
-    end
-
-    def url endpoint
-      "#{API_URL}/#{API_VERSION}/#{endpoint}.#{FORMAT}"
-    end
-
-    def request url, params={}
-      params["key"] = @key
-      result = JSON::parse(RestClient::Request.execute(
-        method: :get,
-        url: url + '?' + URI.encode_www_form(params),
-        params: params,
-        timeout: HTTP_TIMEOUT
-        )
-      )
-
-      if result.is_a?(Hash) && result["error"]
-        raise Errors::Request.new(result["error"]["code"],
-                                  result["error"]["message"])
-      else
-        result
       end
     end
 
@@ -64,6 +40,30 @@ module Apixu
 
     def search query
       request url(:search), q: query
+    end
+
+    private def url endpoint
+      "#{API_URL}/#{API_VERSION}/#{endpoint}.#{FORMAT}"
+    end
+
+    private def request url, params={}
+      params["key"] = @key
+      result = JSON::parse(RestClient::Request.execute(
+          method: :get,
+          url: url + '?' + URI.encode_www_form(params),
+          params: params,
+          timeout: HTTP_TIMEOUT
+        )
+      )
+
+      if result.is_a?(Hash) && result["error"]
+        raise Errors::Request.new(
+          result["error"]["code"],
+          result["error"]["message"]
+        )
+      end
+
+      result
     end
   end
 end
--- spec/apixu_spec.rb
+++ spec/apixu_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Apixu::Client do
-  def env_client
-    Apixu::Client.new ENV["TEST_APIXU_KEY"]
-  end
-
   it 'has a version number' do
     expect(Apixu::VERSION).not_to be nil
   end
@@ -13,61 +9,44 @@ describe Apixu::Client do
     expect(Apixu::Client.new "key").to be_an_instance_of(Apixu::Client)
   end
 
-  it 'stores API key' do
-    api_key = "dummy key"
-    client = Apixu::Client.new api_key
-    expect(client.key).to eq(api_key)
+  def client
+    Apixu::Client.new ENV["APIXUKEY"]
   end
 
-  it 'supports API key environment variable' do
-    api_key = "dummy key"
-    old_key = ENV["APIXU_KEY"]
-    ENV["APIXU_KEY"] = api_key
-    client = Apixu::Client.new
-    ENV["APIXU_KEY"] = old_key
-    expect(client.key).to eq(api_key)
-  end
-
-  it 'does not allow overriding key' do
-    client = Apixu::Client.new "dummy key"
+  it 'raises an error with empty API key' do
     expect do
-      client.key = "another key"
-    end.to raise_error(NameError)
+      Apixu::Client.new ""
+    end.to raise_error(Apixu::Errors::InvalidKey)
   end
 
-  it 'constructs urls' do
-    url = "https://api.apixu.com/v1/test.json"
-    expect(env_client.url :test).to eq(url)
-  end
-
-  it 'supports for conditions list' do
-    result = env_client.conditions
+  it 'retrieves conditions list' do
+    result = client.conditions
     expect(result).to match_json_schema("conditions")
   end
 
-  it 'supports for querying current weather' do
-    result = env_client.current "Paris"
+  it 'retrieves current weather by query' do
+    result = client.current "Paris"
     expect(result).to match_json_schema("current")
   end
 
-  it 'supports for weather forecasts' do
-    result = env_client.forecast "Paris", days=3
+  it 'retrieves weather forecasts by query and number of days' do
+    result = client.forecast "Paris", days=3
     expect(result).to match_json_schema("forecast")
   end
 
-  it 'supports for weather history' do
-    result = env_client.history "Paris", since=Date.today()
+  it 'retrieves historical weather information by query and date' do
+    result = client.history "Paris", since=Date.today()
     expect(result).to match_json_schema("history")
   end
 
-  it 'supports for weather history error' do
+  it 'raises error if history date is invalid' do
     expect {
-      env_client.history "Paris", since=""
+      client.history "Paris", since=""
     } .to raise_error(ArgumentError)
   end
 
-  it 'supports for search' do
-    result = env_client.search "London"
+  it 'it searches for locations' do
+    result = client.search "London"
     expect(result).to match_json_schema("search")
   end
 end
